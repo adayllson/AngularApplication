c25961aff8a66cc771b879ffecd8e3429fc6a940
adailson-almeida <adayllson.almeida@gmail.com>
2022-12-21 11:22:35 -0300

    criação do product service

--- frontend/src/app/components/products/product-create/product-create.component.ts
+++ frontend/src/app/components/products/product-create/product-create.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { ProductService } from './../../product/product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
 
+  constructor(private productService: ProductService, private router: Router) { }
+  
+  ngOnInit(): void {
+    
+  }
+
+  createProduct(): void {
+    this.productService.showMessage('Produto Criado!')
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
